c5bf727877079ba3e1dbe771c404fecb4fd975dd
Giacomo Tartari <giacomo.tartari@gmail.com>
2024-03-24 16:15:45 +0100

    refactor users

--- users/users.go
+++ users/users.go
@@ -12,26 +12,29 @@ type User struct {
 
 // New returns a new User.
 func New(name, email string) (User, error) {
-	// variables can be defined with tea var keyword
-	var newUser = User{
+	err := validateEmail(email)
+	if err != nil {
+		return User{}, fmt.Errorf("email validation failed: %w", err)
+	}
+	newUser := User{
 		Name:  name,
 		Email: email,
 	}
-	// mmm this seems too easy, maybe we should validate the input before
-	// returning the new user. There should be a function somewhere...
 	return newUser, nil
 }
 
-// validateEmail check if the email is valid.
-// This function is very simple, do you think you can improve it?
+// String implements the Stringer interface for the User type.
+// This is how a Go type implements methods
+func (u User) String() string {
+	return fmt.Sprintf("Name: %s, Email: %s", u.Name, u.Email)
+}
+
+// validateEmail checks if the email is valid.
+// We all know that the true way to validate an email address is to send a
+// confirmation email, but this will do for now.
 func validateEmail(email string) error {
-	if !strings.Contains(email, "@") {
+	if strings.Contains(email, "@") == false {
 		return fmt.Errorf("email must contain '@' symbol")
 	}
 	return nil
-}
-
-// String implements the Stringer interface for the User type.
-func (u User) String() string {
-	return fmt.Sprintf("Name: %s, Email: %s", u.Name, u.Email)
 }
